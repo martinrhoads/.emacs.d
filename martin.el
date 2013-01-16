70c129d362982a0fc22161f0b32f8cdd52a3ff15
Martin Rhoads <martin.rhoads@airbnb.com>
2013-01-16 11:33:56 -0800

    highlight colums after 80

--- martin.el
+++ martin.el
@@ -10,19 +10,23 @@
 (add-hook 'emms-player-started-hook 'emms-show)
 (setq
  emms-info-asynchronously t
-  later-do-interval 0.0001
-   emms-info-functions '(emms-info-libtag)
-    emms-source-file-default-directory "~/Music"
-     emms-mode-line-format " %s "
-      emms-show-format "NP: %s")
+ later-do-interval 0.0001
+ emms-info-functions '(emms-info-libtag)
+ emms-source-file-default-directory "~/Music"
+ emms-mode-line-format " %s "
+ emms-show-format "NP: %s")
 
-      (defun next-ten-lines ()
-        (interactive) (dotimes (number 10)
-            (next-line)))
-            (defun previous-ten-lines ()
-              (interactive) (dotimes (number 10)
+(defun next-ten-lines ()
+  (interactive) (dotimes (number 10)
+                  (next-line)))
+(defun previous-ten-lines ()
+  (interactive) (dotimes (number 10)
                   (previous-line)))
 
-                  (global-set-key (kbd "M-n") 'next-ten-lines)
-                  (global-set-key (kbd "M-p") 'previous-ten-lines)
+(global-set-key (kbd "M-n") 'next-ten-lines)
+(global-set-key (kbd "M-p") 'previous-ten-lines)
 
+
+(require 'whitespace)
+(setq whitespace-stype '(face empty tabs lines-tail trailing))
+(global-whitespace-mode t)
