c93667ff1587e609d453fe123e35e4f93704e99a
Martin Rhoads <martin.rhoads@airbnb.com>
2013-03-09 22:17:15 -0800

    add rinari

--- martin.el
+++ martin.el
@@ -1,5 +1,16 @@
 
 ;; shit in my vendor dir
+
+;; Interactively Do Things (highly recommended, but not
+;; strictly required)
+(require 'ido)
+(ido-mode t)
+
+;; Rinari
+(add-to-list 'load-path "~/.emacs.d/vendor/rinari")
+(require 'rinari)
+
+
 (add-to-list 'load-path "~/.emacs.d/vendor/emms/lisp")
 (require 'emms-setup)
 (require 'emms-info-libtag)
