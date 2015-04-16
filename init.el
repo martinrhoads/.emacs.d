9df14d506aecfe03686aa2504bf9978ab5eeca30
Martin Rhoads <ermal14@gmail.com>
2015-04-16 10:29:37 -0700

    add go and column-number-mode

--- init.el
+++ init.el
@@ -9,9 +9,13 @@
   (package-refresh-contents))
 
   ;; Add in your own as you wish:
-  (defvar my-packages '(rspec-mode ssh tramp markdown-mode yaml-mode )
+  (defvar my-packages '(rspec-mode ssh tramp markdown-mode yaml-mode go-mode )
     "A list of packages to ensure are installed at launch.")
 
     (dolist (p my-packages)
       (when (not (package-installed-p p))
           (package-install p)))
+
+
+(setq line-number-mode t)
+(setq column-number-mode t)
