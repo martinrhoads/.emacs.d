77c7d7e3d5f127d824aaf0d20fdbacbedd85de36
martin rhoads <mrhoads@slack-corp.com>
2020-10-22 16:51:03 -0700

    remove emms

--- martin.el
+++ martin.el
@@ -1,19 +1,5 @@
-;; shit in my vendor dir
-(add-to-list 'load-path "~/.emacs.d/vendor/emms/lisp")
-(require 'emms-setup)
-(require 'emms-info-libtag)
-(emms-devel)
-(emms-default-players)
-(push 'emms-player-mplayer emms-player-list)
-(push 'emms-player-mplayer-playlist emms-player-list)
-(add-hook 'emms-player-started-hook 'emms-show)
-(setq
- emms-info-asynchronously t
- later-do-interval 0.0001
- emms-info-functions '(emms-info-libtag)
- emms-source-file-default-directory "~/Music"
- emms-mode-line-format " %s "
- emms-show-format "NP: %s")
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 
 (defun next-ten-lines ()
   (interactive) (dotimes (number 10)
