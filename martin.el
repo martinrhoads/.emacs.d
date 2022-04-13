5da64dd6d2b9d77d11f34f386adf51e05a8d8881
martin rhoads <mrhoads@slack-corp.com>
2022-04-12 21:07:20 -0700

    set tabs to 4; recentf

--- martin.el
+++ martin.el
@@ -3,11 +3,15 @@
 
 (defun next-ten-lines ()
   (interactive) (dotimes (number 10)
-    (next-line)))
+                  (next-line)))
 (defun previous-ten-lines ()
   (interactive) (dotimes (number 10)
-    (previous-line)))
+                  (previous-line)))
 
 (global-set-key (kbd "M-n") 'next-ten-lines)
 (global-set-key (kbd "M-p") 'previous-ten-lines)
 
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
